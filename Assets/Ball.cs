73b02e8cb9ed06597614e557d73c9ed9dd0308f0
KevinBiu <38356483+KevinBiu@users.noreply.github.com>
2018-04-13 11:08:54 -0400

    Add files via upload

---
+++ Assets/Ball.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    public Rigidbody2D rb;
+
+    public float releaseTime;
+
+    private bool isPressed = false;
+
+    void Update()
+    {
+        if (isPressed)
+        {
+            rb.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
+
+
+
+    void OnMouseDown()
+    {
+        isPressed = true;
+        rb.isKinematic = true;
+    }
+
+    void OnMouseUp()
+    {
+        isPressed = false;
+        rb.isKinematic = false;
+
+        StartCoroutine(Release());
+    }
+
+
+    IEnumerator Release()
+    {
+        yield return new WaitForSeconds(releaseTime);
+
+
+        GetComponent<SpringJoint2D>().enabled = false;
+    }
+
+
+
+
+
+}
+
